317d3d028ce2bf122d99aff020e8339341d3fce4
Ilídio Martins <ilidio.martins@gmail.com>
2024-02-08 23:19:55 +0000

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Bypass-Glassdoor-Login-Restriction-To-See-Reviews
+The extension bypasses Glassdoor's login restriction by making a simple modification to the website's url. To use it, the user should navigate to the following url:
+
+https://tinyurl.com/y3w482095
+
+This will take the user directly to the reviews section without requiring them to log in. The extension is made for Google Chrome's store and can be easily installed from there with bypass_glassdor_login.crx
---
+++ src/content.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+    document.getElementById("ContentWallHardsell").innerHTML = "ContentWallHardsell";
+    let r="html,body{overflow:auto !important;}";
+    let s=document.createElement("style");
+    s.type="text/css";
+    s.appendChild(document.createTextNode(r));
+    document.body.appendChild(s);
+    void 0;
+/******/ })()
+;
+//# sourceMappingURL=content.js.map
